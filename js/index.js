7e3cbd158a3b965a2d10c460e8f1135eb4673b0b
@YoussefGad97 <mryoussefaymangad@gmail.com>
2023-04-24 17:29:32 +0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,59 @@
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+// carousel
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
+
+
+
+
+
+// call Function for contact buttons 
+function callNumber() {
+  window.location.href = "tel:+201029344133"; // Replace +1234567890 with the phone number you want to call
+}
+
+
+
+
+
+// email function for buzz me button
+function sendEmail() {
+  window.location.href = "mailto:gad@agconslt.com";
+}
+
+
+
+
+
+
+
+
